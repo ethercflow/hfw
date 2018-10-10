9a02970375c60c084bb1f5752172c3f451743dd8
Zwb <ethercflow@gmail.com>
2018-10-10 03:41:45 -0400

    refactor(hfw): add debug function

--- main.c
+++ main.c
@@ -6,7 +6,16 @@ extern void bar(void);
 typedef void (*funcptr_t)(void);
 extern funcptr_t __start_mcount_loc, __stop_mcount_loc;
 
+static void debug(void);
+
 int main(int argc, char *argv[])
+{
+    debug();
+
+    return 0;
+}
+
+static void debug(void)
 {
     funcptr_t *call_p;
 
@@ -21,6 +30,4 @@ int main(int argc, char *argv[])
 
     fprintf(stderr, "foo+4: %p, bar+4: %p, main+9: %p\n",
 	&foo+4, &bar+4, &main+9);
-
-    return 0;
 }
